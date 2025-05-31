f41ce3287b622822699ad0565b4c378996b0fb7e
Ferperemero86 <fernandoperezmerono@gmail.com>
2025-05-31 06:58:24 +0100

    Move port value to server. Not a constant

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import "dotenv/config";
 
 import app from "./app.js";
 
-import { PORT } from "./shared/constants.js";
+export const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
